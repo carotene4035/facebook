2cb5b85ac9a0594bf241b2e27cedea537daba166
carotene512 <karoten512@gmail.com>
2017-06-24 19:17:03 +0900

    ユーザ一覧デザイン終了。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+
+  before_action :set_user
+
   def index
     @users = User.all
   end
+
+  private
+    def set_user
+      @user = current_user
+    end
 end
