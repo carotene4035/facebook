ba6845b3799368188cb93190c2ee492e189b9ca2
carotene512 <karoten512@gmail.com>
2017-06-24 22:09:14 +0900

    ログイン周りのデザイン修正終了。

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
-  root 'top#index'
+  root 'topics#index'
 
   resources :users, only: [:index, :show]
 
