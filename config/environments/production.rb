66b7ac48e3bbfa5dd9253e35c00ab2b9dd71c28b
carotene512 <karoten512@gmail.com>
2017-06-25 11:13:10 +0900

    本番環境のsendgridの設定を変更。

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,13 +77,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://agile-caverns-34194.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'http://13.59.202.194/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
     {
       user_name: ENV['SENDGRID_USERNAME'],
       password: ENV['SENDGRID_PASSWORD'],
-      domain: "heroku.com",
+      domain: "13.59.202.194",
       address: "smtp.sendgrid.net",
       port: 587,
       authentication: :plain,
