5882c02eb781d8884f1ba774b74b59115d6c7314
carotene512 <karoten512@gmail.com>
2017-06-25 15:38:34 +0900

    コメント削除・コメント編集機能を追加。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  before_action :set_user
+  before_action :set_comment, only: ['edit', 'update', 'destroy']
+
   def create
     @comment = current_user.comments.build(comment_params)
     @topic = @comment.topic
@@ -13,8 +16,42 @@ class CommentsController < ApplicationController
     end
   end
 
+
+  def edit
+    @topic = Topic.find(params[:topic_id])
+  end
+
+  def update
+    @topic = @comment.topic
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to topic_path(@topic), notice: 'コメントを更新しました。' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def destroy
+    @topic = @comment.topic
+    if @comment.destroy
+      redirect_to topic_path(@topic), notice: 'コメントの削除に成功しました。'
+    else
+      redirect_to topic_path(@topic), notice: 'コメントの削除に失敗しました。'
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:topic_id, :content)
   end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def set_user
+    @user = current_user
+  end
 end
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-    def profile_img(user)
+  def profile_img(user)
     return image_tag(user.avatar, alt: user.name, class: 'profile_img') if user.avatar?
 
     unless user.provider.blank?
@@ -8,5 +8,16 @@ module ApplicationHelper
       img_url = 'no_image.png'
     end
     image_tag(img_url, alt: user.name, class: 'profile_img')
+  end
+
+  # ajaxでpostするかどうかを判断する関数
+  def ajax_or_not
+    if action_name == 'show'
+      # コメント追加画面はajaxで投稿
+      return true
+    elsif action_name == 'edit'
+      # コメント編集画面はnot ajax
+      return false
+    end
   end
 end
